ace3f79ea445bce0495abf85d627a7a39f7d4665
Dmitriy Borisovets <gagat@tut.by>
2019-11-21 13:29:22 +0300

    Initial commit

---
+++ src/main/kotlin/by/zmitserkoskinen/spring/statemachineexample/StateMachineExampleApplication.kt
@@ -0,0 +1,11 @@
+package by.zmitserkoskinen.spring.statemachineexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StateMachineExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<StateMachineExampleApplication>(*args)
+}
---
+++ src/test/kotlin/by/zmitserkoskinen/spring/statemachineexample/StateMachineExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.zmitserkoskinen.spring.statemachineexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StateMachineExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
